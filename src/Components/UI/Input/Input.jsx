6f1f8ada9676a13f21f2c3bb83751050c0c0a8f4
ErickAriasC <eariascardona0@soy.sena.edu.co>
2024-08-27 20:02:51 -0500

    conexion con el back_end

--- src/Components/UI/Input/Input.jsx
+++ src/Components/UI/Input/Input.jsx
@@ -15,12 +15,12 @@
 import React from 'react'
 import './Input.css'
 
-const Input = (content) => {
+const Input = (content,{value,set}) => {
   return (
       <div className='form_inputs'>
 
          <label className='form_label'>
-          <input type='text' placeholder=' ' className='form_input' />
+          <input type='text' placeholder=' ' className='form_input' value={value} onChange={(e)=>setNombre (e.target.value)} />
           <span  className='form_text'>{content.name}</span>
          </label>
 
